5ea2f4d9b4691c3a65d026765afd39c9c3dc31ce
sdronenko <sdronenko@list.ru>
2021-01-09 23:28:07 +0300

    [upd] скорректировала н3

--- lesson_1/lesson_1.3.py
+++ lesson_1/lesson_1.3.py
@@ -1,6 +1,7 @@
 # 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
 # Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 
-n = int(input("Введите число n: "))
-total = (n + int(str(n) + str(n)) + int(str(n) + str(n) + str(n)))
+n = input("Введите число n: ")
+total = (int(n) + int(n + n) + int(n + n + n))
 print("Сумма чисел n + nn + nnn - %d" % total)
+# print(f'{n} + {n + n} + {n + n + n} = {int(n) + int(n + n) + (n + n + n)}')
